7ea5ff5a45ee279eeb693fe6c227eab5b7e8bcaf
aryaprayoga99 <ap1864434@gmail.com>
2025-05-05 21:18:09 +0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    protected String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public double getBonus() {
+        return 500_000;
+    }
+}
